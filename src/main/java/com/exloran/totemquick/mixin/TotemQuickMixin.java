10545393f65c3cc7aa4446d33eb0d5948a7252f1
Yukile394 <muhamedmuhet@gmail.com>
2025-11-18 03:01:30 +0300

    Create TotemQuickMixin.java

---
+++ src/main/java/com/exloran/totemquick/mixin/TotemQuickMixin.java
@@ -0,0 +1,8 @@
+package com.exloran.totemquick.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftClient.class)
+public class TotemQuickMixin {
+}
