29999681bf55bfc738cfb921400158226b34b588
Yukile394 <muhamedmuhet@gmail.com>
2026-01-30 03:16:44 +0300

    Add ModMenu class for mod configuration screen

---
+++ src/main/java/com/exloran/totemquick/ModMenu.java
@@ -0,0 +1,13 @@
+package com.exloran.totemquick;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import me.shedaniel.autoconfig.AutoConfig;
+
+public class ModMenu implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return parent -> AutoConfig.getConfigScreen(Config.class, parent).get();
+    }
+}
+
