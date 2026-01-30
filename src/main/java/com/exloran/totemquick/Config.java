a28ac9cae23ac1d93f87657301b5567e73390d7d
Yukile394 <muhamedmuhet@gmail.com>
2026-01-30 03:24:03 +0300

    Refactor Config.java by cleaning up comments and imports
    Removed comments and adjusted imports in Config.java

--- src/main/java/com/exloran/totemquick/Config.java
+++ src/main/java/com/exloran/totemquick/Config.java
@@ -1,18 +1,12 @@
 package com.exloran.totemquick;
 
 import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-import net.minecraft.util.Formatting;
+import me.shedaniel.autoconfig.annotation.Config; // Notasyon olan
 
 @Config(name = "totemquick")
 public class Config implements ConfigData {
     public boolean enabled = true;
     public boolean sesliUyari = true;
-    
-    // Mesaj rengi ayarı (String olarak saklayıp Formatting'e çevireceğiz)
     public String uyarirengi = "RED";
-    
-    // Gecikme ayarı (Sunuculardan atılmamak için)
     public int delayTicks = 0;
 }
-
