7d6915b33e5ba609a5ab6cf226226927fffd36aa
Yukile394 <muhamedmuhet@gmail.com>
2026-01-30 06:32:29 +0300

    Add sound alert and warning color configurations
    Added configuration options for sound alerts and warning color.

--- src/main/java/com/exloran/totemquick/TotemQuickConfig.java
+++ src/main/java/com/exloran/totemquick/TotemQuickConfig.java
@@ -6,6 +6,12 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "totemquick")
 public class TotemQuickConfig implements ConfigData {
 
+    // Mod açık / kapalı
     public boolean enabled = true;
 
+    // Totem yoksa sesli uyarı
+    public boolean sesliUyari = true;
+
+    // Totem yok uyarı rengi (red, yellow, green, gold, vs.)
+    public String uyarirengi = "red";
 }
