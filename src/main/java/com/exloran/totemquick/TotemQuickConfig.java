4faa58e59891dbb72a2c0e5bc9cabd1a41f3ad35
Yukile394 <muhamedmuhet@gmail.com>
2026-01-30 22:57:28 +0300

    Update TotemQuickConfig.java

--- src/main/java/com/exloran/totemquick/TotemQuickConfig.java
+++ src/main/java/com/exloran/totemquick/TotemQuickConfig.java
@@ -6,15 +6,13 @@ import me.shedaniel.autoconfig.annotation.Config;
 @Config(name = "totemquick")
 public class TotemQuickConfig implements ConfigData {
 
-    // Mod açık / kapalı
-    public boolean enabled = true;
+// Mod açık / kapalı  
+public boolean enabled = true;  
 
-    // Totem yoksa sesli uyarı
-    public boolean sesliUyari = true;
+// Totem yoksa sesli uyarı  
+public boolean sesliUyari = true;  
 
-    // Totem yok uyarı rengi (RED, YELLOW, GREEN, GOLD, vb.)
-    public String uyarirengi = "RED";
+// Totem yok uyarı rengi (red, yellow, green, gold, vs.)  
+public String uyarirengi = "red";
 
-    // Rahatlatıcı ses seçimi: Pling, Bell, Chime, XP, LevelUp
-    public String sesSecimi = "Pling";
 }
