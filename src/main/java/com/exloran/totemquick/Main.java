c2046596438e8d2c75e8201675faa6d31bccfba6
Yukile394 <muhamedmuhet@gmail.com>
2026-01-30 06:31:42 +0300

    Change Main class to implement ClientModInitializer

--- src/main/java/com/exloran/totemquick/Main.java
+++ src/main/java/com/exloran/totemquick/Main.java
@@ -2,23 +2,23 @@ package com.exloran.totemquick;
 
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main implements ModInitializer {
+public class Main implements ClientModInitializer {
+
     public static final String MOD_ID = "totemquick";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
-    public void onInitialize() {
-        // Config kaydı
-        AutoConfig.register(Config.class, GsonConfigSerializer::new);
-        
+    public void onInitializeClient() {
+        // ✅ DOĞRU config kaydı
+        AutoConfig.register(TotemQuickConfig.class, GsonConfigSerializer::new);
+
         // Totem mantığını başlat
         TotemManager.init();
-        
-        LOGGER.info("TotemQuick | Baslatildi!");
+
+        LOGGER.info("TotemQuick | Başlatıldı!");
     }
 }
-
