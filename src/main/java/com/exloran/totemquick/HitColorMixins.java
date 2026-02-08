c7f09b27b3f5d9207216ceb7b8e066493ee1625f
Yukile394 <muhamedmuhet@gmail.com>
2026-02-08 08:41:52 +0300

    Update HitColorMixins.java

--- src/main/java/com/exloran/totemquick/HitColorMixins.java
+++ src/main/java/com/exloran/totemquick/HitColorMixins.java
@@ -4,31 +4,34 @@ import com.exloran.totemquick.TotemQuickConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(InGameHud.class)
+@Mixin(Screen.class)
 public abstract class HitColorMixins {
 
     @Inject(method = "render", at = @At("TAIL"))
     private void totemquick$drawCustomHitOverlay(DrawContext context, float tickDelta, CallbackInfo ci) {
         TotemQuickConfig config = AutoConfig.getConfigHolder(TotemQuickConfig.class).getConfig();
+
         if (!config.hitColorEnabled) return;
 
         MinecraftClient client = MinecraftClient.getInstance();
         PlayerEntity player = client.player;
         if (player == null) return;
 
+        // Hasar aldıysa
         if (player.hurtTime > 0) {
             int color = TotemQuickConfig.parseHitColorToRGBA(config.hitColor, config.hitAlpha);
 
             int w = client.getWindow().getScaledWidth();
             int h = client.getWindow().getScaledHeight();
 
+            // Ekranın tamamına overlay
             context.fill(0, 0, w, h, color);
         }
     }
