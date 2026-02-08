ffb26a7741dec495417981302f20402180448b74
Yukile394 <muhamedmuhet@gmail.com>
2026-02-08 08:21:43 +0300

    Update HitColorMixin.java

--- src/main/java/com/exloran/totemquick/HitColorMixin.java
+++ src/main/java/com/exloran/totemquick/HitColorMixin.java
@@ -7,7 +7,7 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixinjection.At;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
