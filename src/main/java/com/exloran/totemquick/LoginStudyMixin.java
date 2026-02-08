faeb692717ea5069e89b919d8fe41fbd985224ed
Yukile394 <muhamedmuhet@gmail.com>
2026-02-08 07:35:34 +0300

    Update LoginStudyMixin.java

--- src/main/java/com/exloran/totemquick/LoginStudyMixin.java
+++ src/main/java/com/exloran/totemquick/LoginStudyMixin.java
@@ -3,6 +3,7 @@ package com.exloran.totemquick.mixin;
 import com.exloran.totemquick.TotemQuickConfig;
 import me.shedaniel.autoconfig.AutoConfig;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.gui.DrawContext;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -10,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 @Mixin(InGameHud.class)
 public abstract class LoginStudyMixin {
 
-    // Hasar alınca çıkan kırmızı vignette rengini config'e göre değiştirir
+    // Hasar alınca çizilen kırmızı overlay rengini değiştirir (1.21 uyumlu)
     @ModifyArg(
-            method = "renderVignette",
+            method = "render",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V"
